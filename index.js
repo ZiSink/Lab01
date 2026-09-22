c52a5d223285e81da6e14fca6f4f93f7d6c428e8
ZiSink <2025166759@estudantes.ips.pt>
2026-09-22 12:15:39 +0100

    adjustments mensagem.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
  const circulo = require("./circulo.js");
  const mensagem = require("./mensagem.js");
 
- 
+mensagem("Olá, mundo!");
 console.log(circulo.area(10)); 
 console.log(circulo.perimetro(5)); 
 console.log(circulo.PI);
--- mensagem.js
+++ mensagem.js
@@ -1,9 +1,4 @@
-const PI = 3.14159; 
-function area(raio) { 
-return PI * raio * raio; 
-} 
-function perimetro(raio) { 
-return PI * 2 * raio; 
-} 
-module.exports.area = area; 
-module.exports.perimetro = perimetro; 
+
+module.exports = function (mensagem) { 
+console.log(mensagem); 
+};
